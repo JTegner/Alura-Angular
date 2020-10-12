2f40271d4a540dbeb4111992a8aec83c2937b916
Jordano Tegner <jordano.tegner@gmail.com>
2020-10-12 11:49:41 -0300

    rotas parametrizadas

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,8 +5,8 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
-import { LoginGuard } from './core/auth/login.guard';
 import { AuthGuard } from './core/auth/auth.guard';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
 
 const routes: Routes = [
     { 
@@ -29,6 +29,10 @@ const routes: Routes = [
         path: 'p/add', 
         component: PhotoFormComponent,
         canActivate: [AuthGuard] 
+    },
+    { 
+        path: 'p/:photoId', 
+        component: PhotoDetailsComponent
     },
     { 
         path: '**', 
