da89ed8ae9068dbb48ad68da5b523f36a95141f0
Jordano Tegner <jordano.tegner@gmail.com>
2020-10-12 21:56:07 -0300

    comentarios como componente

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo';
-import { PhotoComment } from '../photo/photo.comment';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -12,7 +11,7 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoDetailsComponent implements OnInit{ 
 
     photo$: Observable<Photo>;
-    comments$: Observable<PhotoComment[]>;
+    photoId: number;
 
     constructor(
         private route: ActivatedRoute,
@@ -20,13 +19,13 @@ export class PhotoDetailsComponent implements OnInit{
     ) { }
 
     ngOnInit(): void {
-        const photoId = this.route.snapshot.params.photoId;
+        //const photoId = this.route.snapshot.params.photoId;
         //console.log(id);
         //this.photoService
         //    .findById(id)
         //    .subscribe(photo => this.photo = photo);
 
-        this.photo$ = this.photoService.findById(photoId);
-        this.comments$ = this.photoService.getComments(photoId);
+        this.photoId = this.route.snapshot.params.photoId;
+        this.photo$ = this.photoService.findById(this.photoId);
     }
 }
